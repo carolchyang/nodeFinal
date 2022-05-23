12169e24c1492bcc5e690cdacd590880f2d68328
carolchyang <chingheui.yang@gmail.com>
2022-05-23 19:45:19 +0800

    chore: 修正建構後 js 檔案過大問題

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/nodeFinal/" : "/",
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        minSize: 200000,
+        maxSize: 250000,
+      },
+    },
+  },
 });
