ace7a933148241c9e1f940303921b69a30253a1f
carolchyang <chingheui.yang@gmail.com>
2022-06-04 13:10:21 +0800

    fixed: 解決 vuecli 打包問題

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/nodeFinal/" : "/",
   configureWebpack: {
-    optimization: {
-      splitChunks: {
-        minSize: 200000,
-        maxSize: 250000,
+    performance: {
+      hints: "warning",
+      maxEntrypointSize: 50000000,
+      maxAssetSize: 30000000,
+      assetFilter: function (assetFilename) {
+        return assetFilename.endsWith(".js");
       },
     },
   },
