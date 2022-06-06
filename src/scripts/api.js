c826e697f56f1681202516b9ff2e51513b07088a
carolchyang <chingheui.yang@gmail.com>
2022-06-07 06:00:31 +0800

    feat: 修改 header 未帶 Authorization 錯誤

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
-import { getToken } from "@/scripts/methods";
 
 const api = process.env.VUE_APP_API;
-const token = getToken();
-
 // Users
 export const apiUserSignUp = (data) => axios.post(`${api}/users/sign_up`, data);
 export const apiUserSignIn = (data) => axios.post(`${api}/users/sign_in`, data);
@@ -11,25 +8,14 @@ export const apiUpdatePassword = (data) =>
   axios.post(`${api}/users/updatePassword`, data);
 export const apiGetProfile = () => axios.get(`${api}/users/profile`);
 export const apiUpdateProfile = (data) =>
-  axios.post(`${api}/users/profile`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "multipart/form-data",
-    },
-  });
+  axios.post(`${api}/users/profile`, data);
 export const apiGetLikePosts = (data) =>
   axios.get(`${api}/users/likes`, { params: data });
 
 // Post
 export const apiGetPosts = (data) =>
   axios.get(`${api}/article`, { params: data });
-export const apiCreatePost = (data) =>
-  axios.post(`${api}/article`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "multipart/form-data",
-    },
-  });
+export const apiCreatePost = (data) => axios.post(`${api}/article`, data);
 export const apiDelPost = (id) => axios.delete(`${api}/article/${id}`);
 export const apiClickLike = (id) => axios.get(`${api}/article/${id}/likes`);
 export const apiDelLike = (id) => axios.delete(`${api}/article/${id}/likes`);
@@ -42,11 +28,7 @@ export const apiDelComment = (id) => axios.delete(`${api}/comments/${id}`);
 // Forget
 export const apiForgetPassword = (data) => axios.post(`${api}/forget`, data);
 export const apiModifyPassword = (customToken, data) =>
-  axios.post(`${api}/forget/Update`, data, {
-    headers: {
-      Authorization: `Bearer ${customToken}`,
-    },
-  });
+  axios.post(`${api}/forget/Update`, data);
 
 // Follow
 export const apiGetFollow = (data) =>
