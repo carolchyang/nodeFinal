56949838d0321e17de10bbbd1f340383ab8eaa74
carolchyang <chingheui.yang@gmail.com>
2022-06-07 10:33:06 +0800

    style: 修改 layout padding

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="checkSuccess">
     <NavbarComponent @sign-out="signOut"></NavbarComponent>
-    <main class="container py-11">
+    <main class="container pt-11 pb-14 pb-lg-11">
       <div class="row">
         <div class="col-12 col-lg-8">
           <router-view :key="$route.fullPath"></router-view>
